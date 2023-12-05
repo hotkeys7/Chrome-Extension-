b878ddea8296475c39383794cf3aba0c3a34d0b7
hotkeys7 <sstorage268@gmail.com>
2023-12-05 15:31:25 +0100

    Added functionality with Chrome API to save current tab
    Finalised the chrome extension

--- index.js
+++ index.js
@@ -1,36 +1,54 @@
 
 let myLeads = []
 
-let listItems = ""
+const leadsFromLocaStorage = JSON.parse(localStorage.getItem("myLeads") )
+
+if (leadsFromLocaStorage ) {
+    myLeads = leadsFromLocaStorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify( myLeads ) )
+        render(myLeads)
+    })
+})
+
+function render(leads){
+    
+    let listItems = ""
+
+    for (let i = 0; i < leads.length; i++) {
+    
+        listItems +=  
+        `
+        <li>
+            <a target='_blank' href='${leads[i]}'> 
+                ${leads[i]}
+            </a>
+        </li>
+        `
+    }
+    noOrder.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("click", function() {
+    console.log("heeeekkkkkk")
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
 
 inputBtn.addEventListener("click", function() {
     
     myLeads.push(inputu.value)
     inputu.value = ""
-    renderLeads()
 
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+    console.log( localStorage.getItem("myLeads") )
 })
-
-
-function renderLeads(){
-    
-    for (let i = 0; i < myLeads.length; i++) {
-    
-        
-        listItems +=  
-        `
-        <li>
-            <a target='_blank' href='${myLeads[i]}'> 
-                ${myLeads[i]}
-            </a>
-        </li>
-        `
-
-    }
-
-    noOrder.innerHTML = listItems
-  
-}
-
-
 
